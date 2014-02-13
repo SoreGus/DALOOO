90e905bfc7827dc8348336665838ad3a72eadfbf
Jurfest <jurfest.apps@gmail.com>
2014-02-13 11:26:15 -0200

    guerreiro monstro mago

---
+++ DALOOO/Mago.h
@@ -0,0 +1,15 @@
+//
+//  Mago.h
+//  DALOOO
+//
+//  Created by Diego Jurfest Ceccon on 01/01/13.
+//  Copyright (c) 2013 MackenzieApple. All rights reserved.
+//
+
+#import "Personagem.h"
+
+@interface Mago : Personagem
+
+-(id)initWithNome:(NSString *)nome;
+
+@end
---
+++ DALOOO/Monstro.m
@@ -0,0 +1,19 @@
+//
+//  Monstro.m
+//  DALOOO
+//
+//  Created by Diego Jurfest Ceccon on 01/01/13.
+//  Copyright (c) 2013 MackenzieApple. All rights reserved.
+//
+
+#import "Monstro.h"
+
+@implementation Monstro
+
+-(id)initWithNome:(NSString*)nome{
+    self = [super initWithNome:nome ataque:12 defesa:3];
+    
+    return self;
+}
+
+@end
